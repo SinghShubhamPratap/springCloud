eb298025ffe1dbccf0cc08c7cfaefb73cdfbce43
shubham <shubham@localhost>
2021-10-11 21:44:19 +0530

    initial commit

---
+++ PhotoAppAPIGatewayZuul/src/main/java/com/example/PhotoAppAPIGatewayZuul/PhotoAppApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhotoAppAPIGatewayZuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ PhotoAppAPIGatewayZuul/src/test/java/com/example/PhotoAppAPIGatewayZuul/PhotoAppApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhotoAppAPIGatewayZuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
